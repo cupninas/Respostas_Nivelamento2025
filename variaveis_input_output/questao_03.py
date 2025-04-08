80cec503636714010a3e8060e13a4825b1777b91
Marcos Araujo <159856979+marcosnevary@users.noreply.github.com>
2025-04-08 08:53:39 -0300

    Update and rename questao_03.py to variaveis_input_output/questao_03.py

--- variaveis_input_output/questao_03.py
+++ variaveis_input_output/questao_03.py
@@ -1,4 +1,4 @@
-#Crie uma variável que armazene o cálculo de outras duas variáveis e depois mostre o resultado. 
+# Crie uma variável que armazene o cálculo de outras duas variáveis e depois mostre o resultado. 
 
 a = int(input('informe um valor:'))
 b = int(input('informe outro valor:'))
