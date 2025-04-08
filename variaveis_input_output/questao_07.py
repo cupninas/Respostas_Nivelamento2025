b59cb09d2f668ed758e6800e0909bc4a84451aaa
Marcos Araujo <159856979+marcosnevary@users.noreply.github.com>
2025-04-08 08:54:49 -0300

    Update and rename questao_07.py to variaveis_input_output/questao_07.py

--- variaveis_input_output/questao_07.py
+++ variaveis_input_output/questao_07.py
@@ -1,9 +1,11 @@
-#Questão 7
-#Crie um programa que recebe 5 valores, calcule a média aritmética entre eles e mostre o resultado. 
+# Crie um programa que recebe 5 valores, calcule a média aritmética entre eles e mostre o resultado.
+
 a = int(input("Insira o primeiro valor: "))
 b = int(input("Insira o segundo valor: "))
 c = int(input("Insira o terceiro valor: "))
 d = int(input("Insira o quarto valor: "))
 e = int(input("Insira o quinto valor: "))
-media = (a+b+c+d+e)/5 
+
+media = (a + b + c + d + e) / 5 
+
 print("A média é: ", media)
