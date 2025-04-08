197352f28b307b50e1c13774aca1f39fa32ffff4
Nina <110792538+cupninas@users.noreply.github.com>
2025-04-08 00:19:30 -0300

    Create questao_13.py

---
+++ questao_13.py
@@ -0,0 +1,8 @@
+# Faça um programa que peça um número inteiro ao usuário e mostre o seu antecessor e o seu sucessor.
+
+num1 = int(input("Digite um número inteiro: "))
+
+antecessor = num1 - 1
+sucessor = num1 + 1
+
+print(f"Número: {num1} \nAntecessor: {antecessor} \nSucessor: {sucessor}")
