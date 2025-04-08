39afcb69c98b068f9098f76d340e5586228b6be2
Rodrigo Braga <161756897+RodrigoF-Braga@users.noreply.github.com>
2025-04-08 00:26:11 -0300

    Add files via upload

---
+++ Questões_7, 8 e 9.py
@@ -0,0 +1,18 @@
+#Questão 7
+a = int(input("Insira o primeiro valor: "))
+b = int(input("Insira o segundo valor: "))
+c = int(input("Insira o terceiro valor: "))
+d = int(input("Insira o quarto valor: "))
+e = int(input("Insira o quinto valor: "))
+media = (a+b+c+d+e)/5 
+print("A média é: ", media)
+#Questão 8
+a = int(input("Insira o primeiro valor: "))
+b = int(input("Insira o segundo valor: "))
+print("Triplo do primeiro valor: ", a*3)
+print("Triplo do segundo valor: ", b*3)
+#Questão 9
+a = int(input("Insira o valor da base: "))
+b = int(input("Insira o valor da altura: "))
+area = (a * b) / 2 
+print(f"A área é: {area}")
