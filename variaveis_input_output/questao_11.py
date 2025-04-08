91ec3dfb6f0538bae7eadaaab71148e0ece5277b
Marcos Araujo <159856979+marcosnevary@users.noreply.github.com>
2025-04-08 08:58:49 -0300

    Update and rename questao_11.py to variaveis_input_output/questao_11.py

--- variaveis_input_output/questao_11.py
+++ variaveis_input_output/questao_11.py
@@ -1,5 +1,5 @@
 # Faça um programa que peça uma temperatura em Fahrenheit ao usuário, converta para graus Celsius e exiba o resultado.
-#FAÇA UM PROGRAMA QUE PEÇA UMA TEMPERATURA EM FAHRENHEIT AO USUÁRIO, CONVERTA PARA GRAUS CELSIUS E EXIBA O RESULTADO.
+
 fahrenheit = float(input('Digite a temperatura em Fahrenheit: '))
 celsius = (fahrenheit - 32) / 1.8
 
