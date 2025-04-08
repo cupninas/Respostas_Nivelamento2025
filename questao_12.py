6dd4d2c71f23dc63f723b40fd634a2587b4395b3
Nina <110792538+cupninas@users.noreply.github.com>
2025-04-08 00:25:13 -0300

    Add files via upload

---
+++ questao_12.py
@@ -0,0 +1,6 @@
+# Crie um programa que peça uma distância em km, converta para milhas e exiba o resultado.
+# Aproximação de 1 km para 1 milha -> 0.621
+# Convertendo km para float
+
+km = float(input("Coloque um valor em km: "))
+print(km * 0.621)
