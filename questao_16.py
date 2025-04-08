7d3b2d83767dcb9e754eb2da624acfbb2ce0eb80
mauricioaires30 <143522399+mauricioaires30@users.noreply.github.com>
2025-04-08 00:34:28 -0300

    Update questao_16.py

--- questao_16.py
+++ questao_16.py
@@ -1,5 +1,6 @@
 # Faça um programa que receba via teclado o valor de um produto e o valor de desconto.
 # Retorne o valor final a ser pago
+
 valor = float(input("Digite o valor do produto: "))
 desconto = float(input("Digite o valor do desconto: "))
 
