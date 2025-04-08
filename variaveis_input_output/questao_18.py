27cf3660ad6d77f499bd66d5563065fa3c511083
Marcos Araujo <159856979+marcosnevary@users.noreply.github.com>
2025-04-08 09:02:32 -0300

    Update questao_18.py

--- variaveis_input_output/questao_18.py
+++ variaveis_input_output/questao_18.py
@@ -1,10 +1,10 @@
 # Faça um algoritmo que recebe o número total de eleitores de um município, o valor de votos nulos, brancos e válidos. 
 # Ao final, calcule o percentual de cada e mostre na tela.
 
-total_votos = int(input('Informe o total de votantes:'))
-votos_nulos = int(input('Informe o total de votos nulos:'))
-votos_brancos = int(input('Informe o total de votos em branco:'))
-votos_validos = int(input('Informe o total de votos validos:')) 
+total_votos = int(input('Informe o total de votantes: '))
+votos_nulos = int(input('Informe o total de votos nulos: '))
+votos_brancos = int(input('Informe o total de votos em branco: '))
+votos_validos = int(input('Informe o total de votos validos: ')) 
 
 perc_nulos = (votos_nulos * 100) / total_votos
 perc_brancos = (votos_brancos * 100) / total_votos
