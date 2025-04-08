b91d822d35abf2f543d26cad36013324662a931d
mauricioaires30 <143522399+mauricioaires30@users.noreply.github.com>
2025-04-08 00:29:18 -0300

    Add files via upload

---
+++ questao_17.py
@@ -0,0 +1,7 @@
+# Peça um número X e mostre a string ":) " X vezes.
+# Ex.: X = 2, saída será ":) :) ".
+numero = int(input("Digite um número inteiro: "))
+texto = ":) "
+
+novo_texto = texto * numero
+print(novo_texto)
