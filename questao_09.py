73001f3cf372dfe41bf878fb6d2e213618dca3b2
Marcos Araujo <159856979+marcosnevary@users.noreply.github.com>
2025-04-08 08:58:01 -0300

    Update questao_09.py

--- questao_09.py
+++ questao_09.py
@@ -1,6 +1,8 @@
-#Questão 9
-#Sendo a altura de um triângulo 12 e a sua base 2 calcule a área e armazene em uma variável. Dada a fórmula: b*h2
+# Faça um programa que calcula a área de um triângulo.
+
 a = int(input("Insira o valor da base: "))
 b = int(input("Insira o valor da altura: "))
+
 area = (a * b) / 2 
+
 print(f"A área é: {area}")
