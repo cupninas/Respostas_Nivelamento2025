a4b0d63514ffe2b104ec4706e87b4d3d83471435
Marcos Araujo <159856979+marcosnevary@users.noreply.github.com>
2025-04-08 08:59:49 -0300

    Update questao_15.py

--- questao_15.py
+++ questao_15.py
@@ -1,4 +1,4 @@
-#Faça um programa que receba via teclado o seu nome, sua idade e seu curso e exiba no formato: “Olá, [nome]! Você tem [idade] anos e faz [curso]!”.
+# Faça um programa que receba via teclado o seu nome, sua idade e seu curso e exiba no formato: “Olá, [nome]! Você tem [idade] anos e faz [curso]!”.
 
 nome = input("Qual é o seu nome? ")
 idade = int(input("Qual é a sua idade? "))
