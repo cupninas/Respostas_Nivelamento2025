5babc509e52c21b317f1e29d6ab62c56ad7b7c84
Marcos Araujo <159856979+marcosnevary@users.noreply.github.com>
2025-04-08 08:56:23 -0300

    Update and rename questao_08.py to variaveis_input_output/questao_08.py

--- variaveis_input_output/questao_08.py
+++ variaveis_input_output/questao_08.py
@@ -1,6 +1,7 @@
-#Questão 8
-#Crie um programa que recebe 2 valores e calcule o triplo dos dois.
+# Crie um programa que recebe 2 valores e calcule o triplo dos dois.
+
 a = int(input("Insira o primeiro valor: "))
 b = int(input("Insira o segundo valor: "))
+
 print("Triplo do primeiro valor: ", a*3)
 print("Triplo do segundo valor: ", b*3)
