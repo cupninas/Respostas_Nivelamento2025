efc1153e87f043b7799c50a445ad231599c12b4e
Marcos Araujo <159856979+marcosnevary@users.noreply.github.com>
2025-04-08 08:55:51 -0300

    Update questao_05.py

--- variaveis_input_output/questao_05.py
+++ variaveis_input_output/questao_05.py
@@ -1,4 +1,7 @@
 # Receba um número de horas e converta para minutos.
+
 hora = int(input("Digite o número de horas: "))
+
 conv_min = hora * 60
+
 print(f"A conversao de {hora}h para minutos é {conv_min}min")
