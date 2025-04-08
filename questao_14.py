25af912ff794b944a314ecf3b8635b3b6ca72f19
Nina <110792538+cupninas@users.noreply.github.com>
2025-04-08 00:20:30 -0300

    Update questao_14.py

--- questao_14.py
+++ questao_14.py
@@ -1,5 +1,5 @@
 # Um taxista trabalha 8 horas por dia e recebe R$20 por hora.
-# crie um programa que Peça a quantidade de dias por mês que ele trabalha e calcule o seu salário.
+# Crie um programa que peça a quantidade de dias por mês que ele trabalha e calcule o seu salário.
 
 dias = int(input("Quantos dias você trabalhou esse mês? "))
 
