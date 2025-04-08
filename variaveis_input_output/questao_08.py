67323cb1bedc092adb2bbeb5dddf416f047971c6
Marcos Araujo <159856979+marcosnevary@users.noreply.github.com>
2025-04-08 09:01:31 -0300

    Update questao_08.py

--- variaveis_input_output/questao_08.py
+++ variaveis_input_output/questao_08.py
@@ -3,5 +3,5 @@
 a = int(input("Insira o primeiro valor: "))
 b = int(input("Insira o segundo valor: "))
 
-print("Triplo do primeiro valor: ", a*3)
-print("Triplo do segundo valor: ", b*3)
+print("Triplo do primeiro valor: ", a * 3)
+print("Triplo do segundo valor: ", b * 3)
