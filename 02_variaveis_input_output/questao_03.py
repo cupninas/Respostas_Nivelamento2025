ca28b4f5e88e1c8e914219e0bf9c729be584acd6
Nathielly Castro <134755228+nathil@users.noreply.github.com>
2025-04-08 15:23:55 -0300

    Update questao_03.py

--- 02_variaveis_input_output/questao_03.py
+++ 02_variaveis_input_output/questao_03.py
@@ -1,8 +1,8 @@
 # Crie uma variável que armazene o cálculo de outras duas variáveis e depois mostre o resultado. 
 
-a = int(input('informe um valor:'))
-b = int(input('informe outro valor:'))
+valor1 = int(input('informe um valor:'))
+valor2 = int(input('informe outro valor:'))
 
-resultado = a*b 
+resultado = valor1*valor2
 
 print(f'O resultado é de:{resultado}')
