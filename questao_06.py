008f7bc62b7719004f6945a1531faa26f8d60c76
Rodrigo Braga <161756897+RodrigoF-Braga@users.noreply.github.com>
2025-04-08 01:43:48 -0300

    Update questao_06.py

--- questao_06.py
+++ questao_06.py
@@ -1,3 +1,5 @@
+#O setor de RH de uma empresa identificou que o salário médio bruto dos colaboradores é de R$5600,00.
+#Em média, os descontos chegam a 15% desse valor. Calcule e exiba o salário médio líquido dos colaboradores.
 salario_bruto = 5600.00
 percentual_descontos = 15 / 100
 descontos = salario_bruto * percentual_descontos
