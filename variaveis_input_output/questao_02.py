85eaa2aa812186e3ba180ab5df03a930eb0fa7ea
Marcos Araujo <159856979+marcosnevary@users.noreply.github.com>
2025-04-08 08:53:25 -0300

    Update and rename questao_02.py to variaveis_input_output/questao_02.py

--- variaveis_input_output/questao_02.py
+++ variaveis_input_output/questao_02.py
@@ -1,4 +1,4 @@
-#Crie um programa que recebe dois valores e depois troca os valores entre eles. 
+# Crie um programa que recebe dois valores e depois troca os valores entre eles. 
 
 a = int(input('informe um valor:'))
 b = int(input('informe outro valor:'))
