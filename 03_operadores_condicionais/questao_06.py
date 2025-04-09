adb02cda3a5474485a686a872b8954a55f81966d
Marcos Araujo <159856979+marcosnevary@users.noreply.github.com>
2025-04-09 12:44:13 -0300

    Update questao_06.py

--- 03_operadores_condicionais/questao_06.py
+++ 03_operadores_condicionais/questao_06.py
@@ -1,5 +1,5 @@
-#Em um verdureiro, as maçãs custam R$0,50 por unidade e R$0,45 pelo menos 12 unidades são compradas. 
-#Escreva um programa que leia o número de maçãs compradas, calcule e escreva o valor total da compra.
+# Em um verdureiro, as maçãs custam R$0,50 por unidade e R$0,45 pelo menos 12 unidades são compradas. 
+# Escreva um programa que leia o número de maçãs compradas, calcule e escreva o valor total da compra.
 
 macas_compradas = int(input('informe quantas maçãs foram compradas:'))
 
