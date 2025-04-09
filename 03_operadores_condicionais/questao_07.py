dfc769b34743fd6d59e68b003051ee99bdf32362
Marcos Araujo <159856979+marcosnevary@users.noreply.github.com>
2025-04-09 12:46:23 -0300

    Update questao_07.py

--- 03_operadores_condicionais/questao_07.py
+++ 03_operadores_condicionais/questao_07.py
@@ -1,6 +1,7 @@
-# NA UFPA, O CONCEITO FINAL É DECIDIDOPELA MÉDIA ARITMÉTICA DAS 3 NOTAS DE UMALUNO. INFERIOR 
-# A 5:“INSUFICIENTE”.ENTRE 5 E 6.9:“REGULAR”. ENTRE 7 E 8.9: “BOM” MAIOR OU IGUAL A 9: “EXCELENTE". 
-# FAÇA UM PROGRAMA QUE RECEBA TRÊS NOTAS DE UM ALUNO, EXIBA O CONCEITO FINAL JUNTAMENTE DE UMA MENSAGEM.
+# Na UFPA, o conceito final é decidido pela média aritmética das 3 notas de um aluno.
+# Inferior a 5: "Insuficiente". Entre 5 e 6.9: "Regular". Entre 7 e 8.9: "Bom". Maior ou igual a 9: "Excelente".
+# Faça um programa que receba três notas de um aluno, exiba o conceito final juntamente de uma mensagem.
+
 
 nota1 = float(input('insira a primeira nota:'))
 nota2 = float(input('insira a segunda nota:'))
