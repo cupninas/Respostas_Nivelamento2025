71e2652a01f0f6c8a2b0e3a82eec0eabf1beb02b
Nathielly Castro <134755228+nathil@users.noreply.github.com>
2025-04-08 23:48:05 -0300

    Create questao_02.py

---
+++ 3_operadores_condicionais/questao_02.py
@@ -0,0 +1,8 @@
+# Peça ao usuário que insira um número, depois informe na tela se esse valor é par ou ímpar.
+
+numero = int(input('Insira um número:'))
+
+if numero % 2 == 0:
+    print('O número é par.')
+else: 
+    print('O número é ímpar.')
